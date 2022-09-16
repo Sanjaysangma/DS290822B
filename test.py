dc2f635fe7cb1c581458e3c7d131447c71775cf2
Sanjay Sangma <sanjaysangma6@gmail.com>
2022-09-16 17:34:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("Hello World")
+# if (9 < 0) and (0 < -9):
+#     print("hello")
+# elif (9>0) or False:
+#     print("good")
+# else:
+#     print("bad")
+
+# not(3>4)
+
+# not(1&1)
+
+#first-name="Tom"
+
+#numbers=[10,20,30,"python",'java']
+#colors=["Red",'Blue', Green]
+
+print("p" in "Python")
